bc51d6a106336b56f54772f6777d49bf774a7b85
Mariam-Kabu <mariamkabulovi@gmail.com>
2022-01-10 22:22:10 +0400

    some ajax

---
+++ dbh.php
@@ -0,0 +1,8 @@
+<?php
+$dsn = "mysql:host=localhost;dbname=ajax";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ajax";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
